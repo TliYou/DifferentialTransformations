9bb4a416ab98723aa9d2b37baca31e2665c4f34c
Chandrasekar <chandrasekar.sureshkumar@modelon.com>
2014-04-21 13:19:04 -0400

    Changes to include solving DT algorithm symbolically

--- Codes/Ex1.py
+++ Codes/Ex1.py
@@ -12,6 +12,7 @@ import zipfile
 import time
 import shutil
 import scipy.optimize
+import math
 
 try:
   # JModelica
@@ -37,11 +38,32 @@ def run_Ex1(with_plots=True):
 #    os.chdir("C:\JModelica.org-SDK-1.8.1\install\Python\pyjmi\examples")
     curr_dir = os.path.dirname(os.path.abspath(__file__));
     par_dir = os.path.split(curr_dir)[0]
+    
+    # Define model directory
     mo_dir = par_dir+"/Models/Ex1"
-    runDT(mo_dir)
+    
+    # Define model
+    mo_name = mo_dir+"/OPT_Ctl_DT.mo"
+    
+    # Define optimization file
+    mop_file =  mo_dir+"/OPT.mop"
+    
+    # Define optimization problem
+    mop_name = "OPT.Ex1_Opt"
+    
+    # Algorithm parameters
+    degree = 2
+    xe = [0,1,2,3]
 
-def runDT(mo_dir):    
-    casadi_name = compile_fmux("OPT.Ex1_Opt",(mo_dir+"/OPT.mop",mo_dir+"/OPT_Ctl_DT.mo"))  
+    # Call optimization algorithm & solve
+    eq = runDT(mo_dir, mo_name, mop_file, mop_name, degree)
+    print eq
+#     print casadi.getFree(eq[0])
+    solveEqns(eq,xe)
+
+def runDT(mo_dir, mo_name, mop_file, mop_name, degree):    
+#     casadi_name = compile_fmux("OPT.Ex1_Opt",(mo_dir+"/OPT.mop",mo_dir+"/OPT_Ctl_DT.mo"))
+    casadi_name = compile_fmux(mop_name,(mop_file,mo_name))  
     tmpdir = unzip_unit(casadi_name)
     fmux_files = get_files_in_archive(tmpdir)
     xmlFile = fmux_files['model_desc']
@@ -55,32 +77,75 @@ def runDT(mo_dir):
     ocp.parseFMI(xmlFile,options)
 # #    
 # #    
-    print ocp.ode
-    print ocp.u
-    print ocp.x
-    print ocp.y
+#     print ocp.ode
+#     print ocp.u
+#     print ocp.x
+#     print ocp.y
+    
+    # Get states
+    x = SXMatrix(casadi.var(ocp.x[1:ocp.x.size()]))
+#     print x
+    
+    u = ocp.u
+    
+    # Set expansion points as a varible
+    xe = ssym("xe",x.size())
     
     # Get size of input and state
     num_inp = ocp.u.size()
     num_state = ocp.x.size() - 1 # Exclude cost from the state
-     
+         
     # Get RHS of ode
     ode_rhs = SXMatrix(casadi.der(ocp.x)) - ocp.ode
     simplify(ode_rhs)
     cost = ode_rhs[0]
     f = ode_rhs[1:num_state + 1]
     
+    F = DT(f,x,degree,xe)
+    L = DT(cost,x,degree,xe)
+    
 #   Derivative of cost w.r.t inputs  
-    derCost_derU = casadi.jacobian(cost,SXMatrix(casadi.var(ocp.u)))
+#     derCost_derU = casadi.jacobian(cost,SXMatrix(casadi.var(ocp.u)))   
+    derCost_derU = casadi.jacobian(L,SXMatrix(casadi.var(ocp.u)))
     simplify(derCost_derU)
-    print derCost_derU
+#     print derCost_derU
 
 #   Derivative of system dynamics w.r.t inputs     
-    dfdu = casadi.jacobian(f,SXMatrix(casadi.var(ocp.u)))
+#     dfdu = casadi.jacobian(f,SXMatrix(casadi.var(ocp.u)))   
+    dfdu = casadi.jacobian(F,SXMatrix(casadi.var(ocp.u)))
     simplify(dfdu)
-    print dfdu
-#     
-#     Cu = ssym('Cu',ocp.x.size()-1,degree+1) # single input
+#     print dfdu
+
+#   Define coefficients of value function as: V(x) = Cv where Cv is an array.    
+    Cv0 = ssym('Cv0') # constant term
+    Cv = ssym('Cv',num_state,degree) # single input, multiple states
+    
+    # Define value function expression
+    V = Cv0
+    for i in range(0,degree):
+        V = V + Cv[:,i]*pow(x-xe,i+1)
+    
+    simplify(V)
+#     print V
+    
+    dVdx = casadi.jacobian(V,x)
+    simplify(dVdx)
+#     print dVdx
+    
+    # Define the equations for solving
+    eq1 = derCost_derU + dVdx*dfdu
+    eq2 = L + dVdx*F
+    
+    simplify(eq1)
+    simplify(eq2)
+    
+#     print eq1
+#     print eq2
+    
+    return [eq1,eq2,V]
+#     print casadi.getFree(eq1)
+#     print casadi.substitute(f,x,xe)
+    
 #     x = SXMatrix.ones(ocp.x.size()-1,degree+1)
 #     x[:,0] = SXMatrix.ones(ocp.x.size()-1)
 #     x[:,1] = SXMatrix(casadi.var(ocp.x[1:ocp.x.size()]))
@@ -147,7 +212,34 @@ def runDT(mo_dir):
 #         X[i] = pow(H,i)/math.factorial(i)*f1
 #         
 #     return X 
-#         
+#      
+
+def DT(f,x,degree,xe):
+        
+    vars = casadi.getFree(f)
+    out = casadi.substitute(f,x,xe)
+    coeffs = casadi.jacobian(f,x);
+
+    for i in range(0,degree):
+        out = out + casadi.substitute(coeffs,x,xe)*pow(x-xe,i+1)/math.factorial(i+1)
+        coeffs = casadi.jacobian(coeffs,x);
+    
+    simplify(out)
+    return out
+
+def solveEqns(f,xe1):
+    BC = 0
+    var = casadi.getFree(f[0])
+    for i in range(1,2): #len(xe):
+        expr = casadi.substitute(f[0],var[3],xe1[i-1])
+        simplify(expr)
+#         x = scipy.optimize.root([expr[0],expr[1],expr[2]-BC],[[1,1,1,1,1,1]], method='krylov', options={'disp': True})
+        print var
+        print f[0]
+        print expr
+    return 0
+#     for i = 
+   
 if __name__ == '__main__':
 #     global degree, H, z, dVdx, expr11, expr22, ocp
 #     degree = 2
